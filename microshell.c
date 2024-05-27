87ce4a0b017338e09169432a001b09dfc18a6cb2
Verona <rverona-@c1r8p1.42sp.org.br>
2024-05-27 12:36:01 -0300

    funcionando

--- microshell.c
+++ microshell.c
@@ -1,84 +1,98 @@
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 #include <string.h>
 
-void	put_two_strings(char *str, char *str2, int fd)
+void	print_error(char *error, char *arg)
 {
-	while (*str)
-		write(fd, str++, 1);
-	if (str2)
+	while (*error)
+		write(2, error++, 1);
+	if (arg)
 	{
-		while (*str2)
-				write(fd, str2++, 1);
+		while(*arg)
+			write(2, arg++, 1);
 	}
-	write(fd, "\n", 1);
+	write(2, "\n", 1);
 }
 
-int		cmd_counter(char **args)
+void	exec(char **argv, char **envp, int fd_in, int i, int temp)
+{
+	argv[i] = NULL;
+	if (fd_in != 0)
+	{
+		dup2(fd_in, STDIN_FILENO);
+		close(fd_in);
+	}
+	execve(argv[temp], &argv[temp], envp);
+	print_error("error: cannot execute ", argv[temp]);
+}
+
+void	shell(char **argv, char **envp, int argc)
 {
 	int	i = 0;
-	int	res = 1;
+	int	fd_in = 0;
+	int	pipefd[2];
+	pid_t	pid;
 
-	while (args[i])
+	while (argv[i])
 	{
-		if (strcmp(args[i], "|") == 0 || strcmp(args[i], ";") == 0)
-			res++;
-		i++;
-	}
-	return (res);
-}
-
-void	child_process(char **argv, int io[2], int size)
-{
-	char	**cmd_table = (char**)malloc(sizeof(char *) * size);
-
-	for (int i = 0; i < size; i++)
-			cmd_table[i] = argv[i];
-	dup2(io[1], 1);
-	execve(argv[0], cmd_table, NULL);
-}
-
-void	shell(char **argv, int cmd_count)
-{
-	int			i;
-	int			temp;
-	int			pipefd[2];
-	pid_t		pid;
-
-	i = -1;
-	if (cmd_count == 1)
-	{
-		pid = fork();
-		if (pid == 0)
-				execve(argv[0], argv, NULL);
-		return ;
-	}
-	while (argv[++i])
-	{
-		if (!strcmp(argv[i], "|"))
+		int temp = i;		
+		while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+			i++;
+		if (!strcmp(argv[temp], "cd"))
 		{
-			argv[i][0] = '\0';
-			temp = i + 1;
-			break;
+			if (!argv[temp + 1] || (argv[temp + 1] && argv[temp + 2] != NULL && strcmp(argv[temp + 2], ";")))
+				print_error("error: cd: bad arguments", NULL);
+			if (chdir(argv[temp + 1]) == -1)
+				print_error("error: cd: cannot change directory to ", argv[temp + 1]);
 		}
+		else if (i != 0 && (argv[i] == NULL || !strcmp(argv[i], ";")))
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				exec(argv, envp, fd_in, i, temp);
+			}
+			else
+			{
+				close(fd_in);
+				while (waitpid(-1, NULL, WUNTRACED) != -1)
+					;
+				fd_in = 0;
+			}
+		}
+		else if (i != 0 && !strcmp(argv[i], "|"))
+		{
+			if (pipe(pipefd) == -1)
+				return ;
+			pid = fork();
+			if (pid == 0)
+			{
+				dup2(pipefd[1], STDOUT_FILENO);
+				close(pipefd[1]);
+				close(pipefd[0]);
+				exec(argv, envp, fd_in, i, temp);
+			}
+			else
+			{
+				if (fd_in != 0)
+					close(fd_in);
+				close(pipefd[1]);
+				fd_in = pipefd[0];
+			}
+		}
+		if (i < argc - 1)
+			i++;
 	}
-	if (pipe(pipefd) == -1)
-			return ;
-
-	pid = fork();
-	if (pid == 0)
-			child_process(argv, pipefd, i);
-	argv += temp;
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	(void)argc;
-	(void)envp;
-	int		cmd_count = cmd_counter(argv);
-
+	if (argc < 2)
+	{
+		print_error("error: wrong arguments", NULL);
+		return (0);
+	}
 	argv++;
-	for (int i = 0; i < cmd_count; i++)
-			shell(argv, cmd_count);
+	shell(argv, envp, argc);
 }
